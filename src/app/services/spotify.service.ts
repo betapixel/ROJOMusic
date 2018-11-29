c0fe531cd4cbef6e2bb5a0b988e5f86646004c0e
Alejandro Arias <alejoarias@gmail.com>
2018-11-29 13:13:14 -0300

    Modifcaciones autorización

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,39 +1,26 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
-
   constructor(private http: HttpClient) {
     console.log('Spotify service listo');
   }
 
+  qetQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+    const headers = new HttpHeaders({ Authorization: 'Bearer BQDOioTIx_JXedmhpGbOnyaH2TNOIh-1Z8qXb5EjjHI_ncTC7OCuYOzL70vOg3KkcZgS_1u78rtYymf5_LU' });
+    return this.http.get(url, { headers });
+  }
+
   getNewReleases() {
-
-    const headers = new HttpHeaders({
-      'Authorization':
-      'Bearer BQBdh396YZL5tVF84_lbQy-lzhk9tCqq7VROXKOTZtqFYwsQrca8RwYZmxm0YbdRR_ionrgaMGe_XU2Ldng'
-    });
-
-
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',
-    {headers}).pipe(map(data => data['albums'].items));
-
+    return this.qetQuery('browse/new-releases?limit=20').pipe(map(data => data['albums'].items));
   }
 
   getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      'Authorization':
-      'Bearer BQBdh396YZL5tVF84_lbQy-lzhk9tCqq7VROXKOTZtqFYwsQrca8RwYZmxm0YbdRR_ionrgaMGe_XU2Ldng'
-    });
-
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
-      {headers}).pipe(map(data => data['artists'].items));
-
-  }
+    return this.qetQuery(`search?q=${termino}&type=artist&limit=15`).pipe(map(data => data['artists'].items));
+  };
 }
